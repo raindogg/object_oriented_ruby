0df042bb64c952c65dd33859a2f53defca19a3b9
wabray17 <wabray17@gmail.com>
2016-08-24 12:11:26 -0500

    Finished kata part 1

--- kata1.rb
+++ kata1.rb
@@ -1,3 +1,28 @@
 input = File.open("weather.dat") 
-array = input.each { |line| p line.split }
-puts array
+array = []
+input.each { |line| array << line.split }
+
+smaller = []
+array.each do |line|
+  smaller << line.slice!(0, 3)
+end
+
+smaller.slice!(0, 2)
+
+smallest_range = 100
+final = 0
+
+smaller.each do |arr|
+  day = arr[0]
+  max = arr[1].to_i
+  min = arr[2].to_i
+  range = max - min
+  if range < smallest_range
+    final = day
+    smallest_range = range
+  end
+end
+
+puts "The smallest temperature range occured on day " + final 
+
+
